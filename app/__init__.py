03cf1fdd4f39a8daf7e24c77f2598ff972a040b7
MRichardN <mathengerichard29@gmail.com>
2019-01-09 20:46:19 +0300

    [bug] fix missing blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask import Flask
 #Local import
 from instance.config import app_config
 #import instance.config.app_config
+from app.api.v1.views.views import version1
 
 
 
@@ -16,6 +17,7 @@ def create_app(config_name):
     Create app.
     """
     app = Flask(__name__, instance_relative_config=True)
+    app.register_blueprint(version1)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     return app
