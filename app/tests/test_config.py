8409ae0d26e5cef7a745ed4de81744a5629f5f03
MRichardN <mathengerichard29@gmail.com>
2019-01-11 08:54:34 +0300

    [Chore#163140448] Add test for development

--- app/tests/test_config.py
+++ app/tests/test_config.py
@@ -4,8 +4,22 @@ from app import create_app
 class TestDevelopmentConfig(unittest.TestCase):
     """ Test development environment config."""
 
+    def setUp(self):
+        # Initialize development environment in app
+        self.app = create_app('development')
+
+    def test_development_environment(self):
+        """ Test configuration in developmenet environment."""
+        self.assertEqual(self.app.config['DEBUG'], True)  
+
+    def tearDown(self):
+        #Restore app to initial state
+        self.app = None      
+
 class TestTestingConfig(unittest.TestCase):
     """ Test testing environment config."""
+
+    
 
 class TestStagingConfig(unittest.TestCase):
     """ Test statging environment config."""
