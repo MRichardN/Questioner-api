69109ce9ddd3547ee2b33fc0ee42f9661bbca38e
MRichardN <mathengerichard29@gmail.com>
2019-01-11 08:23:39 +0300

    [starts#163140448]Add config classes

---
+++ app/tests/test_config.py
@@ -0,0 +1,14 @@
+import unittest
+from app import create_app
+
+class TestDevelopmentConfig(unittest.TestCase):
+    pass
+
+class TestTestingConfig(unittest.TestCase):
+    pass
+
+class TestStagingConfig(unittest.TestCase):
+    pass
+
+class TestProductionConfig(unittest.TestCase):
+    pass    
