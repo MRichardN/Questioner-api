2d5151d9cac0ade413babc3a9d42ed4c11f956ec
MRichardN <mathengerichard29@gmail.com>
2019-01-14 22:49:42 +0300

    [chore#163203804] Modify comments in questions model

--- app/api/v1/models/questions_model.py
+++ app/api/v1/models/questions_model.py
@@ -1,45 +1,31 @@
-class Question:
-    """
-    This class represents the questions model.
-    """
-    def __init__(self, question_list):
-        """
-        Initialize questions list.
-        """
-        self.question_list = question_list
+from datetime import datetime
+from .base_model import Model
+from ..utils.utils import idGenerator
 
-    def show_questions(self):
-        """
-        Show all questions.
-        """
-        return self.question_list
+questions = []
 
-    def add_question(self, question):
-        """
-        Add questions to the list.
-        """
-        self.question_list.append(question)
+class Question(Model):
+    """ This class represents the class model."""
 
-    def update_question(self, index, createdOn, createdBy, meetup, title, body, votes):
-        """
-        Update a question.
-        """
-        new_question = [new_question for new_question in self.question_list if new_question["id"] == index]
-        if new_question:
-            new_question[0]["createdOn"] = createdOn
-            new_question[0]["createdBy"] = createdBy
-            new_question[0]["meetup"] = meetup
-            new_question[0]["title"] = title
-            new_question[0]["body"] = body
-            new_question[0]["votes"] = votes
-            return new_question[0]
+    def __init__(self):
+        super().__init__(questions)
 
-    def delete_question(self, index):
-        """
-        Delete questions.
-        """
-        del_question = [del_question for del_question in self.question_list if del_question["id"] == index]
-        if del_question:
-            self.question_list.remove(del_question[0])
-            return True
+    def save(self, data):
+        """ Save meetup."""
+        data['id'] = idGenerator(self.collection)
+        data['votes'] = 0
+        return super().save(data)
 
+    def upvote(self, question_id):
+        """ upvote a question."""
+        for question in questions:
+            if question['id'] == question_id:
+                question['votes'] = question['votes']+1
+            return question    
+
+    def downvote(self, question_id):
+        """ Downvote a question."""
+        for question in questions:
+            if question['id'] == question_id:
+                question['votes'] = question['votes']-1
+            return question    
