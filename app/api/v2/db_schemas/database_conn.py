946b031c73d100a093e70eb43c9474c4d2cd7418
MRichardN <mathengerichard29@gmail.com>
2019-01-17 10:29:06 +0300

    [chore#163281901] add db conn string

---
+++ app/api/v2/db_schemas/database_conn.py
@@ -0,0 +1,30 @@
+
+import os
+import psycopg2
+from instance.config import app_config
+
+
+config_name = os.getenv('APP_ENV')
+config = app_config[config_name]
+
+
+def db_conn():
+    """ DB connection string."""
+
+    database = config.DEV_DB_NAME
+    host = config.DB_HOST
+    user = config.DB_USERNAME
+    password = config.DB_PASSWORD
+    
+
+    db_url = 'dbname={} host={} user={} password={}'.format(database, host, user, password)
+
+    try:
+        conn = psycopg2.connect(db_url)
+
+    except Exception as e:
+        print('Unable to connect to the database', e)
+
+    return conn        
+
+        
